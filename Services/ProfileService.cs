d93457325741c0862e5862d412f56f2f7092090e
Raman <2391616@cognizant.com>
2025-03-28 10:26:04 +0000

    Updated Specific Product

---
+++ Services/ProfileService.cs
@@ -0,0 +1,30 @@
+using System.Net.Http.Json;
+using BlazorBootstrap;
+using EComBlazor.AuthServices;
+using EComBlazor.Models;
+using EComBlazor.Services;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+
+namespace EComBlazor.Services{
+
+    public class ProfileService{
+        private readonly HttpClient _httpClient;
+        private AuthService AuthService;
+
+        public ProfileService(HttpClient httpClient){
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<UserProfile> GetUserProfileAsync(int userId)
+        {
+            await AuthService.SetAuthHeaderAsync();
+            // Makes an HTTP GET request to fetch the user profile from the API.
+            return await _httpClient.GetFromJsonAsync<UserProfile>($"api/userprofiles/{userId}");
+        }
+
+    
+    }
+
+}
